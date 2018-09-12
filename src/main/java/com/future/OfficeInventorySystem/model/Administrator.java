af65e3c7f00daf69914826f1801b315a2b90f6ea
priagungs <priagungsatyagama@gmail.com>
2018-09-12 16:50:27 +0700

    implement administrator model, add JPA and postgres to pom.xml

--- src/main/java/com/future/OfficeInventorySystem/model/Administrator.java
+++ src/main/java/com/future/OfficeInventorySystem/model/Administrator.java
@@ -1,4 +1,33 @@
 package com.future.OfficeInventorySystem.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.TableGenerator;
+
+@Entity
+@TableGenerator(name = "Administrator", initialValue = 13516000)
 public class Administrator {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter @Setter
+    private Long NIP;
+
+    @Getter @Setter
+    private String name;
+
+    @Getter @Setter
+    private String username;
+
+    @Getter @Setter
+    private String picture; //path to an image in filesystem
+
+    @Getter @Setter
+    private String password;
+
 }
