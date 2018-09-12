23c17a2e4c24a013fdbdfeec38dd92fd4c3d698d
priagungs <priagungsatyagama@gmail.com>
2018-09-12 17:09:51 +0700

    implemented employee model

--- src/main/java/com/future/OfficeInventorySystem/model/Employee.java
+++ src/main/java/com/future/OfficeInventorySystem/model/Employee.java
@@ -1,4 +1,38 @@
 package com.future.OfficeInventorySystem.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@SequenceGenerator(name = "employee_nip", initialValue = 16516000)
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "employee_nip")
+    @Getter @Setter
+    private Long NIP;
+
+    @Getter @Setter
+    private String name;
+
+    @Getter @Setter
+    private String username;
+
+    @Getter @Setter
+    private String picture; //path to an image in filesystem
+
+    @Getter @Setter
+    private String password;
+
+    @Getter @Setter
+    private String division;
+
+    @Getter @Setter
+    private String role;
+
+    @Getter @Setter
+    private Long superiorNIP;
+
 }
