8ef1ff1f4b540cde2cb486db9b6c693cad93b52c
Ricky Kennedy <13516105@std.stei.itb.ac.id>
2018-09-15 21:05:43 +0700

    add Item, ItemTransaction, and Transaction model

--- src/main/java/com/future/OfficeInventorySystem/model/ItemTransaction.java
+++ src/main/java/com/future/OfficeInventorySystem/model/ItemTransaction.java
@@ -1,4 +1,21 @@
 package com.future.OfficeInventorySystem.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Id;
+
 public class ItemTransaction {
+
+    @Id
+    @Setter @Getter
+    private Long transactionId;
+
+    @Id
+    @Setter @Getter
+    private Long itemId;
+
+    @Id
+    @Setter @Getter
+    private Long boughtQty;
 }
--- src/main/java/com/future/OfficeInventorySystem/model/Transaction.java
+++ src/main/java/com/future/OfficeInventorySystem/model/Transaction.java
@@ -1,4 +1,29 @@
 package com.future.OfficeInventorySystem.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@TableGenerator(name = "transaction_id" , initialValue = 18216000)
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "transaction_id")
+    @Setter @Getter
+    private Long transactionId;
+
+    @Setter @Getter
+    private Date date;
+
+    @Setter @Getter
+    private String supplier;
+
+    @Setter @Getter
+    private Long idAdministrator;
+
 }
