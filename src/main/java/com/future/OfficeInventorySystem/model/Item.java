675428337d0de187560cd7e0acf2e181a21f3d49
Ricky Kennedy <13516105@std.stei.itb.ac.id>
2018-09-15 20:39:07 +0700

    add dependency

--- src/main/java/com/future/OfficeInventorySystem/model/Item.java
+++ src/main/java/com/future/OfficeInventorySystem/model/Item.java
@@ -1,4 +1,34 @@
 package com.future.OfficeInventorySystem.model;
 
+
 public class Item {
+
+    private String itemID;
+
+    private String name;
+
+    private String picture;
+
+    private Integer price;
+
+    private Integer totalQty;
+
+    private Integer avaibleQty;
+
+    private String description;
+
+    public Item (String itemID, String name,
+                 String picture, Integer price,
+                 Integer totalQty, Integer avaibleQty,
+                 String description) {
+        this.itemID = itemID;
+        this.name = name;
+        this.picture = picture;
+        this.price = price;
+        this.totalQty = totalQty;
+        this.avaibleQty = avaibleQty;
+        this.description = description;
+    }
+
+
 }
