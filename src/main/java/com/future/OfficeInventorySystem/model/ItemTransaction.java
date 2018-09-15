dcf837c12cb069223a00b3edd1c257c3f786f5da
Ricky Kennedy <13516105@std.stei.itb.ac.id>
2018-09-15 21:17:58 +0700

    add Item, Transaction, and ItemTransaction

--- src/main/java/com/future/OfficeInventorySystem/model/ItemTransaction.java
+++ src/main/java/com/future/OfficeInventorySystem/model/ItemTransaction.java
@@ -3,8 +3,11 @@ package com.future.OfficeInventorySystem.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 
+
+@Entity
 public class ItemTransaction {
 
     @Id
