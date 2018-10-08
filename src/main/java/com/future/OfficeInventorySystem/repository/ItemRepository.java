7b02653565778e194ae550953768ca465bf4a97f
shintaayuck <shinta990405@gmail.com>
2018-10-08 12:05:28 +0700

    item repository done

--- src/main/java/com/future/OfficeInventorySystem/repository/ItemRepository.java
+++ src/main/java/com/future/OfficeInventorySystem/repository/ItemRepository.java
@@ -2,8 +2,22 @@ package com.future.OfficeInventorySystem.repository;
 
 import com.future.OfficeInventorySystem.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Query("select i from Item i where i.idItem = idItem")
+    Item findByIdItem(Long idItem);
+
+    @Query("select i from Item i where i.itemName = itemName")
+    List<Item> findByItemName(String itemName);
+
+    @Query("select i from Item i where i.availableQty > 0")
+    List<Item> findAllByAvailableQty();
+
+
 }
--- src/main/java/com/future/OfficeInventorySystem/repository/ItemTransactionRepository.java
+++ src/main/java/com/future/OfficeInventorySystem/repository/ItemTransactionRepository.java
@@ -2,8 +2,10 @@ package com.future.OfficeInventorySystem.repository;
 
 import com.future.OfficeInventorySystem.model.ItemTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ItemTransactionRepository extends JpaRepository<ItemTransaction, Long> {
+
 }
