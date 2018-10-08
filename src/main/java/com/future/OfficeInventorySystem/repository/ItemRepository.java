7a05acc3456ee4045de485e02217eeab9d00fa2f
priagungs <priagungsatyagama@gmail.com>
2018-10-08 11:52:00 +0700

    create item and request repo

--- src/main/java/com/future/OfficeInventorySystem/repository/ItemRepository.java
+++ src/main/java/com/future/OfficeInventorySystem/repository/ItemRepository.java
@@ -1,4 +1,10 @@
 package com.future.OfficeInventorySystem.repository;
 
-public class ItemRepository {
+
+import com.future.OfficeInventorySystem.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
 }
--- src/main/java/com/future/OfficeInventorySystem/repository/RequestRepository.java
+++ src/main/java/com/future/OfficeInventorySystem/repository/RequestRepository.java
@@ -1,4 +1,11 @@
 package com.future.OfficeInventorySystem.repository;
 
-public class RequestRepository {
+import com.future.OfficeInventorySystem.model.Request;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request,Long> {
+
+    Request findRequestByIdRequest(Long idRequest);
 }
