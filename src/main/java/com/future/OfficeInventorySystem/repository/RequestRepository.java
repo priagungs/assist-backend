593c5748cf570d6739433da6158a7f0b50044ed1
priagungs <priagungsatyagama@gmail.com>
2018-10-14 15:46:26 +0700

    add pagination in request

--- src/main/java/com/future/OfficeInventorySystem/repository/RequestRepository.java
+++ src/main/java/com/future/OfficeInventorySystem/repository/RequestRepository.java
@@ -3,6 +3,8 @@ package com.future.OfficeInventorySystem.repository;
 import com.future.OfficeInventorySystem.model.Request;
 import com.future.OfficeInventorySystem.model.Status;
 import com.future.OfficeInventorySystem.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,8 +15,8 @@ import java.util.Set;
 public interface RequestRepository extends JpaRepository<Request,Long> {
 
     Request findRequestByIdRequest(Long idRequest);
-    List<Request> findAllRequestByUser(User user);
-    List<Request> findAllRequestByStatus(Status status);
-    List<Request> findAllRequestByStatusAndSuperior(Status status, User superior);
+    Page<Request> findAllRequestByUser(User user, Pageable pageable);
+    Page<Request> findAllRequestByStatus(Status status, Pageable pageable);
+    Page<Request> findAllRequestByStatusAndSuperior(Status status, User superior, Pageable pageable);
 
 }
