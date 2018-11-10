8e71d9ec6d9b25046cd39562d65647b63fe8268c
priagungs <priagungsatyagama@gmail.com>
2018-11-10 16:10:09 +0700

    fix security

--- controller/src/main/java/com/future/office_inventory_system/configuration/WebSecurityConfiguration.java
+++ controller/src/main/java/com/future/office_inventory_system/configuration/WebSecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.future.office_inventory_system.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.future.office_inventory_system.model.User;
 import com.future.office_inventory_system.service.UserAuthService;
+import com.future.office_inventory_system.value_object.LoggedinUserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,12 +11,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     UserAuthService userAuthService;
+
+    @Autowired
+    LoggedinUserInfo loggedinUserInfo;
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -27,11 +34,22 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
                 .formLogin()
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/login-detail", true)
+                    .loginPage("/api/login")
+                    .successHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+                        httpServletResponse.setContentType("application/json;charset=UTF-8");
+                        ObjectMapper mapper = new ObjectMapper();
+                        String json = mapper.writeValueAsString(loggedinUserInfo.getUser());
+                        httpServletResponse.getWriter().print(json);
+                    })
+                    .failureHandler((httpServletRequest, httpServletResponse, e) -> httpServletResponse
+                            .setStatus(HttpServletResponse.SC_UNAUTHORIZED))
                     .permitAll()
                 .and()
-                .logout().permitAll()
+                .logout()
+                    .logoutSuccessHandler((httpServletRequest, httpServletResponse, authentication) ->
+                            httpServletResponse.setStatus(HttpServletResponse.SC_OK))
+                    .permitAll()
                 .and()
                 .httpBasic()
                 .and()
--- controller/src/main/java/com/future/office_inventory_system/controller/FileController.java
+++ controller/src/main/java/com/future/office_inventory_system/controller/FileController.java
@@ -16,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class FileController {
 
     @Autowired
