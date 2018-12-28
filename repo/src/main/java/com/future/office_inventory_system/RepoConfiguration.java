35b57457aafe92cb95e1385217b46194f8b5b884
rickykennedy25 <13516105@std.stei.itb.ac.id>
2018-12-28 18:22:09 +0700

    finished init unit testing repo

---
+++ repo/src/main/java/com/future/office_inventory_system/RepoConfiguration.java
@@ -0,0 +1,15 @@
+package com.future.office_inventory_system;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class RepoConfiguration {
+
+    public static void main(String[] args) {
+        run(RepoConfiguration.class, args);
+    }
+
+}
