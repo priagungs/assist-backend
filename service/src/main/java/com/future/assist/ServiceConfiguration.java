216aa03efa18139ef3256f6bd02aa66d776c006d
rickykennedy25 <13516105@std.stei.itb.ac.id>
2018-12-30 16:01:32 +0700

    init service unit test

---
+++ service/src/main/java/com/future/assist/ServiceConfiguration.java
@@ -0,0 +1,13 @@
+package com.future.assist;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class ServiceConfiguration {
+
+    public static void main (String[] args) {
+        run(ServiceConfiguration.class,args);
+    }
+}
---
+++ service/src/test/java/com/future/assist/service/service_impl/UserServiceImplTest.java
@@ -0,0 +1,60 @@
+package com.future.assist.service.service_impl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void createUser() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void updateUser() {
+        assertTrue(true);
+
+    }
+
+    @Test
+    public void readAllUsers() {
+        assertTrue(true);
+
+    }
+
+    @Test
+    public void readUserByIdUser() {
+        assertTrue(true);
+
+    }
+
+    @Test
+    public void readAllUsersByIdSuperior() {
+        assertTrue(true);
+
+    }
+
+    @Test
+    public void readAllUsersContaining() {
+        assertTrue(true);
+
+    }
+
+    @Test
+    public void readUserByUsername() {
+        assertTrue(true);
+
+    }
+
+    @Test
+    public void deleteUser() {
+        assertTrue(true);
+
+    }
+}
