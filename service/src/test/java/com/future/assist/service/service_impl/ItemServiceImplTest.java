3103c6c8202d26fdf9bf7b0f0ac7e1e6f28034fd
shintaayuck <shinta990405@gmail.com>
2019-01-03 22:14:34 +0700

    Add itemService unit test

--- service/src/test/java/com/future/assist/service/service_impl/ItemServiceImplTest.java
+++ service/src/test/java/com/future/assist/service/service_impl/ItemServiceImplTest.java
@@ -3,9 +3,15 @@ package com.future.assist.service.service_impl;
 import com.future.assist.exception.ConflictException;
 import com.future.assist.exception.InvalidValueException;
 import com.future.assist.exception.NotFoundException;
+import com.future.assist.model.RequestStatus;
 import com.future.assist.model.entity_model.Item;
+import com.future.assist.model.entity_model.Request;
+import com.future.assist.model.entity_model.User;
 import com.future.assist.repository.ItemRepository;
 import com.future.assist.service.service_interface.ItemService;
+import com.future.assist.service.service_interface.RequestService;
+import com.future.assist.service.service_interface.UserHasItemService;
+import com.future.assist.service.service_interface.UserService;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +23,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -32,9 +40,21 @@ public class ItemServiceImplTest {
 
     @MockBean
     private ItemRepository itemRepository;
+    
+    @MockBean
+    private RequestService requestService;
+    
+    @MockBean
+    private LoggedinUserInfo loggedinUserInfo;
 
+    @MockBean
+    private UserHasItemService userHasItemService;
+    
     private Item item1;
     private Item item2;
+    private Request request1;
+    private Request request2;
+    private User user;
 
     @Before
     public void setUp() {
@@ -59,6 +79,15 @@ public class ItemServiceImplTest {
         item2.setRequests(new ArrayList<>());
         item2.setOwners(new ArrayList<>());
         item2.setIsActive(true);
+        
+        request1 = new Request();
+        request1.setRequestStatus(RequestStatus.APPROVED);
+        
+        request2 = new Request();
+        request2.setRequestStatus(RequestStatus.SENT);
+        
+        user = new User();
+        user.setIdUser(1L);
     }
 
     @Test(expected = ConflictException.class)
@@ -97,16 +126,19 @@ public class ItemServiceImplTest {
         itemService.updateItem(item2);
     }
 
-//    @Test(expected = ConflictException.class)
-//    public void updateItemConflictException() {
-//        Item newItem = item1;
-//        newItem.setItemName("false name");
-//        Mockito.when(itemRepository.findByIdItemAndIsActive(item1.getIdItem(), true))
-//            .thenReturn(Optional.of(newItem));
-//        Mockito.when(itemRepository.findByItemNameIgnoreCaseAndIsActive(item1.getItemName(), true))
-//            .thenReturn(Optional.of(item1));
-//        itemService.updateItem(item1);
-//    }
+    @Test(expected = ConflictException.class)
+    public void updateItemConflictException() {
+        Item newItem = new Item();
+        newItem.setIdItem(item1.getIdItem());
+        newItem.setItemName("false name");
+        newItem.setAvailableQty(item1.getAvailableQty());
+        newItem.setTotalQty(item1.getTotalQty());
+        Mockito.when(itemRepository.findByIdItemAndIsActive(item1.getIdItem(), true))
+            .thenReturn(Optional.of(newItem));
+        Mockito.when(itemRepository.findByItemNameIgnoreCaseAndIsActive(item1.getItemName(), true))
+            .thenReturn(Optional.of(item1));
+        itemService.updateItem(item1);
+    }
 
     @Test(expected = InvalidValueException.class)
     public void updateItemInvalidValueTest2() {
@@ -215,11 +247,19 @@ public class ItemServiceImplTest {
         itemService.deleteItem(item1.getIdItem());
     }
 
-//    @Test
-//    public void deleteItemSuccess() {
-//        Mockito.when(itemRepository.findByIdItemAndIsActive(item1.getIdItem(), true))
-//            .thenReturn(Optional.of(item1));
-//
-//        Assert.assertEquals(ResponseEntity.ok().build(), itemService.deleteItem(item1.getIdItem()));
-//    }
+    @Test
+    public void deleteItemSuccess() {
+        Mockito.when(itemRepository.findByIdItemAndIsActive(item1.getIdItem(), true))
+            .thenReturn(Optional.of(item1));
+        List<Request> requests = new ArrayList<>();
+        requests.add(request1);
+        requests.add(request2);
+        Mockito.when(requestService.readAllRequestsByItem(item1)).thenReturn(requests);
+        Mockito.when(requestService.updateRequestByRequestObject(request1)).thenReturn(request1);
+        Mockito.when(requestService.updateRequestByRequestObject(request2)).thenReturn(request2);
+        Mockito.when(loggedinUserInfo.getUser()).thenReturn(user);
+        Mockito.when(userHasItemService.readAllUserHasItemsByIdItem(item1.getIdItem(), PageRequest.of(0,
+            Integer.MAX_VALUE))).thenReturn(new PageImpl<>(new ArrayList<>()));
+        Assert.assertEquals(ResponseEntity.ok().build(), itemService.deleteItem(item1.getIdItem()));
+    }
 }
